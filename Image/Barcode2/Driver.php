928a8ebab08db116643c810910e51a8571ffab29
Daniel O'Connor <daniel.oconnor@gmail.com>
2011-12-11 23:11:34 +0800

    Add interface for issue #5

---
+++ Image/Barcode2/Driver.php
@@ -0,0 +1,19 @@
+<?php
+interface Image_Barcode2_Driver 
+{
+
+    /**
+     * Draws a barcode
+     *
+     * @param string $text A text that should be in the image barcode
+     * @return image            The corresponding image barcode
+     */
+    public function draw($text);
+
+    /**
+     * Set the image rendering library.
+     *
+     * @param Image_Barcode2_Writer $writer Library to use.
+     */
+    public function setWriter(Image_Barcode2_Writer $writer);
+}
