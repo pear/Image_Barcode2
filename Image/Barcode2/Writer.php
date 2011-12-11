3818e18e381ed681c33815d7330f85ce37d9c7db
Daniel O'Connor <daniel.oconnor@gmail.com>
2011-12-12 01:25:26 +1030

    Add more methods to adapter for issue #3

--- Image/Barcode2/Writer.php
+++ Image/Barcode2/Writer.php
@@ -6,19 +6,32 @@
  * @todo See if there are other packages doing this already
  */
 class Image_Barcode2_Writer {
-   public function imagestring($image, $font, $x, $y, $string, $color) {
-      return imagestring($image, $font, $x, $y, $string, $color;
-   }
+    
+    public function imagecreate($width, $height) {
+        return imagecreate($width, $height);
+    }
 
-   public function imagefill($image, $x, $y, $color) {
-      return imagefill($image, $x, $y, $color);
-   }
+    public function imagestring($image, $font, $x, $y, $string, $color) {
+        return imagestring($image, $font, $x, $y, $string, $color;
+    }
 
-   public function imagefilledrectangle($image, $x1, $y1, $x2, $y2, $color) {
-      return imagefilledrectangle($image, $x1, $y1, $x2, $y2, $color);
-   }
+    public function imagefill($image, $x, $y, $color) {
+        return imagefill($image, $x, $y, $color);
+    }
+
+    public function imagefilledrectangle($image, $x1, $y1, $x2, $y2, $color) {
+        return imagefilledrectangle($image, $x1, $y1, $x2, $y2, $color);
+    }
    
-   public function imagestring($image, $font, $x , $y, $string, $color) {
-      return imagestring($image, $font, $x , $y, $string, $color);
-   }
+    public function imagestring($image, $font, $x , $y, $string, $color) {
+        return imagestring($image, $font, $x , $y, $string, $color);
+    }
+
+    public function imagefontheight($font) {
+        return imagefontheight($font);
+    }
+
+    public function imagecolorallocate($image, $red, $green, $blue) {
+        return imagecolorallocate($image, $red, $green, $blue);
+    }
 }
